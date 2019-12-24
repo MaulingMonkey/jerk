11844b9c84f333a36b4579ee46886a41bb3762b7
MaulingMonkey <git@maulingmonkey.com>
2019-12-24 15:13:54 -0800

    Fix/extend JDK detection logic to be more arch aware.
    * Windows was preferring 32-bit installs regardless of target.
    * Windows was unable to find JDK 13, or it's jvm.dll s.
    * Linux was only finding x86_64 installs.
    * Unit tests assumed javah.exe was a thing, but it isn't in JDK 13.

--- scripts/test.cmd
+++ scripts/test.cmd
@@ -1,21 +1,21 @@
 @pushd "%~dp0.." && setlocal
-@call :build
-@call :test
+
+cargo +stable-x86_64 build --all
+cargo +stable-x86_64 test  --all
+
+cargo +stable-i686 build --all
+cargo +stable-i686 test  --all
+
+@set RUSTUP_TOOLCHAIN=
 @call :doc "%~dp0..\jerk"
 @call :doc "%~dp0..\jerk-build"
 @call :doc "%~dp0..\jerk-test"
+
 @where wsl >NUL 2>NUL && wsl bash --login -c scripts/test.sh
+
 @popd && endlocal && goto :EOF
 
 
-
-:build
-cargo build --all
-@exit /b %ERRORLEVEL%
-
-:test
-cargo test --all
-@exit /b %ERRORLEVEL%
 
 :doc
 @pushd "%~1"
