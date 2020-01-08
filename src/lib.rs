01dc212a7c29eedd034d7cea6948ffd38869c6fc
MaulingMonkey <git@maulingmonkey.com>
2020-01-08 03:25:52 -0800

    Remove dlopen dependency to improve build times by a couple seconds.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,6 @@
 #![cfg_attr(feature = "nightly", feature(external_doc)  )] // https://doc.rust-lang.org/unstable-book/language-features/external-doc.html
 #![cfg_attr(feature = "nightly", doc(include = "../Readme.md"))]
 
-#[macro_use] extern crate dlopen_derive;
-
 mod env;
 #[allow(dead_code)] mod jar;    // TODO: Make public after finalizing APIs?
 #[allow(dead_code)] mod java;   // TODO: Make public after finalizing APIs?
