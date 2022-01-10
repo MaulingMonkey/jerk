a6030ed63da0fcd72c65b11c0192f0a862bf4f13
MaulingMonkey <git@maulingmonkey.com>
2022-01-10 08:25:56 -0800

    fix bitrot (bumps MSRV to 1.54)
    - external_doc was removed in favor of doc = include_str!(...)
    - doc = include_str!(...) breaks 1.36.0, even when guarded by cfg_attr
    - crate-type = "rlib" now necessary to avoid build errors about multiple std versions
    - extern crate ...; now broken and unnecessary
    - whatever JVM I'm using seems to have an existing VM at start

--- example-hello-world-jar/tests/test.rs
+++ example-hello-world-jar/tests/test.rs
@@ -1,5 +1,3 @@
-extern crate example_hello_world_jar; // Force it to be built/used
-
 #[test] fn test() {
     jerk::run_test!("com.maulingmonkey.jerk.example_hello_world_jar", "Adder",  "test");
     jerk::run_test!("com.maulingmonkey.jerk.example_hello_world_jar", "Global", "test");
--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,5 @@
 use crate::*;
+use crate::jvm::JniError;
 use jni_sys::*;
 use std::convert::*;
 use std::fmt::{self, Debug, Display, Formatter};
@@ -51,36 +52,38 @@ impl From<String> for JavaTestError {
 
 #[doc(hidden)]
 pub fn run_test_impl(jar: &str, package: &str, class: &str, method: &str) -> Result<()> {
+    let mut reused_vm = false;
     {
         let mut vm = VM.lock().unwrap();
         if vm.is_null() {
-            **vm = create_java_vm(jar);
+            **vm = create_java_vm(jar, &mut reused_vm);
         }
     }
 
     let env = test_thread_env();
     if env == null_mut() { return Err("Couldn't initialize Java VM".into()); }
-    
+
     let class_id    = format!("{}/{}\0", package.replace(".", "/"), class);
     let method_id   = format!("{}\0", method);
-    
+
     // Safety:
     // * `**env` must be valid (non-null, not dangling, valid fn pointers if present)
     // * string IDs must be `\0` terminated
     unsafe {
-        let class_id    = (**env).FindClass.unwrap()(env, class_id.as_ptr() as *const _);
-        assert_ne!(
-            class_id,
-            null_mut(),
-            concat!(
-                "Failed to FindClass {}.{}.  Possible causes:\n",
-                "  - Typos in the package or class name.\n",
-                "  - The corresponding .jar may have been built with a newer JDK (are you mixing old and new JDKs for 32-bit and 64-bit?)\n",
-                "  - The corresponding .jar may not be have been found (are you using `jerk_build::metabuild()` in your build.rs?)\n",
-            ),
-            package,
-            class
-        );
+        let class_id = (**env).FindClass.unwrap()(env, class_id.as_ptr() as *const _);
+        if class_id.is_null() {
+            panic!(
+                concat!(
+                    "Failed to find class {package}.{class}.  Possible causes:\n",
+                    "{reused_vm_warning}",
+                    "  - Typos in the package or class name.\n",
+                    "  - The corresponding .jar may have been built with a newer JDK (are you mixing old and new JDKs for 32-bit and 64-bit?)\n",
+                    "  - The corresponding .jar may not be have been found (are you using `jerk_build::metabuild()` in your build.rs?)\n",
+                ),
+                package=package, class=class,
+                reused_vm_warning = if reused_vm { "  - WARNING: jerk::run_test! was unable to start a new JVM / specify a java.class.path.  The .jar may not have been loaded!\n" } else { "" },
+            );
+        }
         let method_id   = (**env).GetStaticMethodID.unwrap()(env, class_id, method_id.as_ptr() as *const _, "()V\0".as_ptr() as *const _);
         assert_ne!(method_id, null_mut(), "Failed to GetStaticMethodID {}.{}", class, method);
         (**env).CallStaticVoidMethodA.unwrap()(env, class_id, method_id, [].as_ptr());
@@ -116,14 +119,25 @@ fn attach_current_thread() -> *mut JNIEnv {
     env as *mut _
 }
 
-fn create_java_vm(jar: &str) -> *mut JavaVM {
-    JVM.create_java_vm(vec![
+fn create_java_vm(jar: &str, reused_vm: &mut bool) -> *mut JavaVM {
+    match JVM.create_java_vm(vec![
         //"-verbose:class".to_string(),
         //"-verbose:jni".to_string(),
         "-ea".to_string(),  // Enable Assertions
         "-esa".to_string(), // Enable System Assertions
         format!("-Djava.class.path={}", jar),
-    ]).unwrap()
+    ]) {
+        Err(JniError::EXIST) => {
+            if let Some(vm) = JVM.get_created_java_vms().unwrap().into_iter().next() {
+                *reused_vm = true;
+                vm
+            } else {
+                panic!("JNI_EEXIST error creating Java VM, but unable to get an existing VM");
+            }
+        },
+        Err(err) => panic!("JNI error creating Java VM: {}", err),
+        Ok(vm) => vm
+    }
 
 }
 
